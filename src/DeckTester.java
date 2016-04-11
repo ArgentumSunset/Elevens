2ec397342ac0db24eea2540459357fedbc35d649
Colter Giem <giemcl@s.dcsdk12.org>
2016-04-11 10:02:43 -0600

    Finished Activity 2

--- src/DeckTester.java
+++ src/DeckTester.java
@@ -8,6 +8,18 @@ public class DeckTester {
 	 *	@param args is not used.
 	 */
 	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		String[] ranks = {"jack", "queen", "king"};
+		String[] suits = {"black", "red"};
+		int[] pointValues = {10, 11, 12};
+		Deck d = new Deck(ranks, suits, pointValues); 
+		System.out.println(d.isEmpty());
+		System.out.println(d.size());
+		System.out.println(d.deal());
+		System.out.println(d.deal());
+		System.out.println(d.deal());
+		System.out.println(d.deal());
+		System.out.println(d.deal());	
+		System.out.println(d.deal());
+		System.out.println(d.deal());
 	}
 }
